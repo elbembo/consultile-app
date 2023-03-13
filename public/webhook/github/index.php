b494212282c8f212fa0632de1db0d39b3e47cb08
Emad <elbembo@gmail.com>
2023-03-13 23:03:11 +0200

    .

--- public/webhook/github/index.php
+++ public/webhook/github/index.php
@@ -1,14 +1,20 @@
 <?php
-function execPrint($command) {
-    $result = array();
-    exec($command, $result);
-    // print("<pre>");
-    // foreach ($result as $line) {
-    //     print($line . "\n");
-    // }
-    // print("</pre>");
-    return $result ;
+if ($_SERVER['method'] == 'POST') {
+    $json = file_get_contents('php://input');
+    function execPrint($command)
+    {
+        $result = array();
+        exec($command, $result);
+        // print("<pre>");
+        // foreach ($result as $line) {
+        //     print($line . "\n");
+        // }
+        // print("</pre>");
+        return $result;
+    }
+    $data = execPrint("git pull");
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($data);
+}else{
+    http_response_code(405);
 }
-$data = execPrint("git pull");
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode($data);
