56a4ec63cde046a7b42447a838fb73e2c9449d25
Emad <elbembo@gmail.com>
2023-03-15 06:46:42 +0200

    d

--- resources/views/layouts/user_type/auth.blade.php
+++ resources/views/layouts/user_type/auth.blade.php
@@ -43,7 +43,7 @@
 
         @else
             @include('layouts.navbars.auth.sidebar')
-            <main class="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg {{ (Request::is('rtl') ? 'overflow-hidden' : '') }}">
+            <main class="main-content position-relative height-vh-100 h-100  border-radius-lg {{ (Request::is('rtl') ? 'overflow-hidden' : '') }}">
                 @include('layouts.navbars.auth.nav')
                 <div class="container-fluid py-4">
                     @yield('content')
