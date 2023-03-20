ac792bd1282070b05b17ff2fe94d1f8cc6f914dc
Emad <elbembo@gmail.com>
2023-03-20 16:52:50 +0200

    s

--- app/Exports/ContactsExport.php
+++ app/Exports/ContactsExport.php
@@ -6,10 +6,12 @@ use App\Models\Contact;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
-class ContactsExport implements FromCollection,ShouldAutoSize,WithStyles, WithHeadings
+
+class ContactsExport  extends \PhpOffice\PhpSpreadsheet\Cell\StringValueBinder implements FromCollection, ShouldAutoSize, WithStyles, WithHeadings, WithColumnFormatting
 {
     use Exportable;
     public function collection()
@@ -26,16 +28,27 @@ class ContactsExport implements FromCollection,ShouldAutoSize,WithStyles, WithHe
     }
     public function headings(): array
     {
-        return ["Id",'Salutation',
-        'First Name',
-        'Last Name',
-        'Work E-mail',
-        'Work Phone',
-        'Mobile',
-        'Company',
-        'Position',
-        'Country',
-        'Tags',
-        'Source','Subscribe'];
+        return [
+            "Id",
+            'Salutation',
+            'First Name',
+            'Last Name',
+            'Work E-mail',
+            'Work Phone',
+            'Mobile',
+            'Company',
+            'Position',
+            'Country',
+            'Tags',
+            'Source',
+            'Subscribe'
+        ];
+    }
+    public function columnFormats(): array
+    {
+        return [
+            'B' => NumberFormat::FORMAT_DATE_DDMMYYYY,
+            'C' => NumberFormat::FORMAT_CURRENCY_EUR_SIMPLE,
+        ];
     }
 }
