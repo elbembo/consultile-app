1dd37422bd7e743004a24d3be1750cc7ed812316
Emad <elbembo@gmail.com>
2023-03-22 08:23:32 +0200

    d

--- app/Events/NotificationEvent.php
+++ app/Events/NotificationEvent.php
@@ -10,13 +10,12 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 class NotificationEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
-    public $username;
     public $message;
-    public function __construct($username)
+    public function __construct($message)
     {
-        $this->username = $username;
-        $this->message  = "{$username} send you a notification";
+        $this->message = $message;
     }
+
     public function broadcastOn()
     {
         //it is a broadcasting channel you need to add this route in channels.php file
