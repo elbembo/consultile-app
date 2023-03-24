0bd6a55f3291a2759c04c77d3c652f751244215e
Emad <elbembo@gmail.com>
2023-03-24 07:24:28 +0200

    D

--- app/Http/Controllers/CampaignReportController.php
+++ app/Http/Controllers/CampaignReportController.php
@@ -46,8 +46,9 @@ class CampaignReportController extends Controller
 
         }
         $contscts = EmailTraker::whereIn('email_trakers.massage_id',$fails)->join('contacts','email_trakers.contact_id','=','contacts.id')->get('email');
+        file_put_contents('fails_emails.txt',"");
         foreach($contscts as $contsct){
-            file_put_contents('/fails_emails.txt',$contsct->email."\n",FILE_APPEND);
+            file_put_contents('fails_emails.txt',$contsct->email."\n",FILE_APPEND);
         }
 
         return view('reports.campaigns.index',compact('contscts'));
