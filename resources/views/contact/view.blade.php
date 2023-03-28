eb23bd7044bfd80a25fb730a12efffe49e5de319
Emad <elbembo@gmail.com>
2023-03-28 07:52:46 +0200

    d

--- resources/views/contact/view.blade.php
+++ resources/views/contact/view.blade.php
@@ -179,12 +179,31 @@
             </div>
         </div>
     </div>
+    <div class="modal fade" id="requestPermissionModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
 @endsection
 @pushOnce('scripts')
 <script>
+    var myModal = new bootstrap.Modal(document.getElementById('myModal'))
 function requestAccess(ele){
     const {event,ask,id,to} = ele.dataset
     post('/requestPermission',{event,ask,id,to}).then(res=>{
+        myModal.show()
         console.log(res)
     })
 
