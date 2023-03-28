c3479d498c60a058b6d4a25dbcb1993780750ae1
Emad <elbembo@gmail.com>
2023-03-28 04:24:55 +0200

    س

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,7 +62,7 @@
             z-index: 999;
         }
         .img-th{
-            min-height: 19.5rem;
+            min-height: 10rem;
             display: flex;
             align-items: center;
             /* padding: 0.5rem; */
