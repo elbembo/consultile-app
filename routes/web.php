8dbd2ecff1d98536e1fbeead70eb7615545dc992
Emad <elbembo@users.noreply.github.com>
2023-04-01 04:43:05 +0200

    dd

--- routes/web.php
+++ routes/web.php
@@ -69,7 +69,7 @@ Route::domain('app.' . env('APP_DOMAIN', 'consultile.com'))->group(function () {
         Route::delete('campaigns/{campaign}/attachments', [CampaignController::class, 'removeAttachment'])->name('campaigns.removeAttachment');
 
         Route::get('subscribes/export', [SubscribeHomeController::class, 'export']);
-        Route::post('subscribes/exportSMS', [SubscribeHomeController::class, 'exportSMS']);
+        Route::post('subscribes/exportSMS', [SubscribeHomeController::class, 'exportSMS'])->name('export.sms');
         Route::resource('subscribes', SubscribeHomeController::class);
 
         Route::resource('users', UserController::class);
