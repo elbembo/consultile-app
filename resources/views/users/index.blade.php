3e151cb6a84a4cda3b7f740f15f423cce60da445
elbembo <elbembo@gmail.com>
2023-09-11 18:32:59 +0300

    d

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -15,7 +15,7 @@
                     <div class="card-header pb-0">
                         <div class="d-lg-flex">
                             <div>
-                                <h5 class="mb-0">All Users</h5>
+                                <h5 class="mb-0">All Users ..</h5>
                             </div>
                             <div class="ms-auto my-auto mt-lg-0 mt-4">
                                 <div class="ms-auto my-auto">
